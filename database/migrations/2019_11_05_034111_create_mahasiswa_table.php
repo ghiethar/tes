115fe4c11b8eb25e3b632ac55cc8506f0b63c0ce
Inzaghi <hasanudin.inzaghi27@gmail.com>
2019-11-05 16:47:44 +0700

    Added To github tes

---
+++ database/migrations/2019_11_05_034111_create_mahasiswa_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMahasiswaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mahasiswa', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->integer('nim');
+            $table->string('namalengkap', 30);
+            $table->enum('gender', ['L', 'P']);
+            $table->text('alamat');
+            $table->integer('idjurusan');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mahasiswa');
+    }
+}
